f1fb6d60e526f9a3094b8dbd9ebbb56081f481b6
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-12-05 13:32:46 -0700

    Diseño basico parte 2

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -5,9 +5,39 @@ class BasicDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+    return Scaffold(
+        body: Column(
+      children: [Image(image: AssetImage('assets/landscape.jpg')), Tittle()],
+    ));
+  }
+}
+
+class Tittle extends StatelessWidget {
+  const Tittle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                'Kandersteg Swizerland',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+        ],
       ),
     );
   }
