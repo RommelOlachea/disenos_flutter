21b0b6878be30a0b0352bb95e3318a65c33e6ff2
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-12-06 11:48:28 -0700

    diseño con scroll parte 1

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -5,10 +5,46 @@ class ScrollScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ScrollScreen'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          //BackGround
+          BackGround(),
+          MainContent()
+        ],
       ),
     );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text('Hola mundo'),
+      ],
+    );
+  }
+}
+
+class BackGround extends StatelessWidget {
+  const BackGround({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: const Color(0xff30BAD6),
+        height: double.infinity,
+        alignment: Alignment.topCenter,
+        child: Image(image: AssetImage('assets/scroll-1.png')));
   }
 }
